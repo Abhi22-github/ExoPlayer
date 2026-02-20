99a29cea0ea6fece90b3f787f2c7b8d5b12eaa7f
Abhi <satpute.abhi22@gmail.com>
2026-02-20 04:02:00 +0000

    changing the view for video list

--- app/src/main/java/com/roaa/exoplayer/VideoListScreen.kt
+++ app/src/main/java/com/roaa/exoplayer/VideoListScreen.kt
@@ -1,22 +1,26 @@
 package com.roaa.exoplayer
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 
 @Composable
 fun VideoListScreen(
@@ -25,57 +29,71 @@ fun VideoListScreen(
     videoItemClick: (VideoItem) -> Unit
 ) {
     Scaffold() {
-        Column(
-            modifier = Modifier.padding(it)
-        ) {
-            LazyColumn() {
-                items(videoFolder.videos) { videoItem ->
-                    VideoItemCard(
-                        videoItem = videoItem,
-                        videoItemClick = {
-                            videoItemClick(it)
-                        })
-                }
+        VideoList(
+            videoList = videoFolder.videos,
+            modifier = Modifier.padding(it),
+            videoItemClick = {
+                videoItemClick(it)
             }
+        )
+    }
+}
+
+@Composable
+fun VideoList(
+    videoList: List<VideoItem>,
+    videoItemClick: (VideoItem) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    LazyVerticalGrid(
+        state = rememberLazyGridState(),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(vertical = 0.dp, horizontal = 16.dp),
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(videoList) {
+            VideoItemView(
+                videoItem = it,
+                videoItemClick = { videoItem ->
+                    videoItemClick(videoItem)
+                })
         }
     }
 }
 
 @Composable
-fun VideoItemCard(
+fun VideoItemView(
     videoItem: VideoItem,
     videoItemClick: (VideoItem) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     val context = LocalContext.current
-    Card(
-        modifier = Modifier
-            .clickable { videoItemClick(videoItem) }
-            .padding(vertical = 8.dp, horizontal = 12.dp)
-    ) {
-        Row(
+    Column() {
+        Card(
             modifier = Modifier
-        ) {
-            AsyncImage(
-                model = ImageRequest.Builder(context = context)
-                    .data(videoItem.uri)
-                    .crossfade(true)
-                    .build(),
-                contentDescription = videoItem.name,
+                .fillMaxSize()
+                .clickable {
+                    videoItemClick(videoItem)
+                }
+                .clip(RoundedCornerShape(12.dp))) {
+            Box(
                 modifier = Modifier
                     .aspectRatio(16f / 9f)
-                    .weight(0.3f),
-                contentScale = ContentScale.FillWidth
-            )
-
-            Column() {
-                Text(
-                    text = videoItem.name
-                )
-                Text(
-                    text = videoItem.duration.toString()
-                )
+                    .clip(RoundedCornerShape(12.dp))
+            ) {
+                ThumbnailImage(videoItem = videoItem)
             }
         }
+        Text(
+            text = videoItem.name, maxLines = 1, overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = videoItem.duration.toString(), maxLines = 1, overflow = TextOverflow.Ellipsis
+        )
     }
 }
+
+
