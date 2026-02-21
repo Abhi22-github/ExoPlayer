b959413e6426774df04003acc378dc2292979f64
Abhi <satpute.abhi22@gmail.com>
2026-02-21 20:12:00 +0530

    Added remember savable to retain the screen for configuration changes

--- app/src/main/java/com/roaa/exoplayer/MainActivity.kt
+++ app/src/main/java/com/roaa/exoplayer/MainActivity.kt
@@ -4,11 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
@@ -23,7 +33,9 @@ class MainActivity : ComponentActivity() {
             ExoPlayerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     val backStack =
-                        remember { mutableStateListOf<Destinations>(Destinations.FolderListScreen) }
+                        rememberSaveable { mutableStateListOf<Destinations>(Destinations.FolderListScreen) }
+                    var previousSize by remember { mutableStateOf(backStack.size) }
+
 
 
                     NavDisplay(
@@ -31,6 +43,33 @@ class MainActivity : ComponentActivity() {
                         onBack = {
                             if (backStack.size > 1) {
                                 backStack.removeLastOrNull()
+                            }
+                        },
+                        transitionSpec = {
+
+
+                            val isForward = backStack.size > previousSize
+
+                            previousSize = backStack.size
+
+                            if (isForward) {
+                                slideInHorizontally(
+                                    initialOffsetX = { it },
+                                    animationSpec = tween(350)
+                                ) + fadeIn() togetherWith
+                                        slideOutHorizontally(
+                                            targetOffsetX = { -it / 4 },
+                                            animationSpec = tween(350)
+                                        ) + fadeOut()
+                            } else {
+                                slideInHorizontally(
+                                    initialOffsetX = { -it / 4 },
+                                    animationSpec = tween(350)
+                                ) + fadeIn() togetherWith
+                                        slideOutHorizontally(
+                                            targetOffsetX = { it },
+                                            animationSpec = tween(350)
+                                        ) + fadeOut()
                             }
                         },
                         entryProvider = entryProvider {
